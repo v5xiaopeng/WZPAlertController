3542a701db5c67abe9dbf930ec8448196a32ebba
mac <mac@zz-rdmac1590.local>
2019-10-17 09:44:04 +0800

    2

---
+++ WZPAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZPAlertController
+//
+//  Created by mac on 2019/10/17.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VersionString=`grep -E 's.version.*=' WZPAlertController.podspec`
+VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+
+NewVersionNumber=$(($VersionNumber + 1))
+LineNumber=`grep -nE 's.version.*=' WZPAlertController.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" WZPAlertController.podspec
+
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+
+git add .
+git commit -am ${NewVersionNumber}
+git tag ${NewVersionNumber}
+git push origin master --tags
+pod repo push WZPRepoSpecs WZPAlertController.podspec --verbose --allow-warnings --use-libraries
+
